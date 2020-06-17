a80c660f195cb071289c775d08b5d101a52944f2
ljmutawa <laila.almutawa2004@gmail.com>
2020-06-17 20:47:24 +0300

    done

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,9 @@
+let family = ["Salma", "Mohamad", "Faisal", "Laila"]
+var evens = [2, 4, 6, 8]
+var grades = [95.2, 93.0, 90.6, 96.0]
+var trueFalse = [true, true, true, false]
+
+print (family[0], family[3])
+print (evens[0], evens[3])
+print (grades[0], grades[3])
+print (trueFalse[0], trueFalse[3])
